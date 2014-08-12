fda7826feb73183dca9904b8d75dfb5471701dec
Marco Inacio <marcoigarapava@gmail.com>
2014-08-12 00:11:25 -0300

    added NaN test for sum

--- src/test/unit/math/matrix/sum_test.cpp
+++ src/test/unit/math/matrix/sum_test.cpp
@@ -1,6 +1,7 @@
 #include <stan/math/matrix/sum.hpp>
 #include <stan/math/matrix/typedefs.hpp>
 #include <gtest/gtest.h>
+#include <boost/math/special_functions/fpclassify.hpp>
 
 TEST(MathMatrix,sum_vector_int) {
   std::vector<int> x(3);
@@ -51,4 +52,31 @@ TEST(MathMatrix,sum_vector_double) {
   m = stan::math::matrix_d(3,2);
   m << 1, 2, 3, 4, 5, 6;
   EXPECT_FLOAT_EQ(21.0,sum(m));
+}
+
+TEST(MathMatrix, sum_nan) {
+  double nan = std::numeric_limits<double>::quiet_NaN();
+
+  Eigen::MatrixXd m1(3,2);
+  m1 << 1, nan,
+        3, 4.1,
+        nan, 6;
+  Eigen::MatrixXd m2(3,2);
+  m2 << 10.1, 100,
+        nan, 0,
+        -10, -12;
+        
+  Eigen::VectorXd v1(3);
+  v1 << 10.1, nan, 1.1;
+        
+  std::vector<double> v2(14, 1.1);
+  v2[7] = nan;
+        
+  using stan::math::sum;
+  using boost::math::isnan;
+    
+  EXPECT_PRED1(isnan<double>, sum(m1));
+  EXPECT_PRED1(isnan<double>, sum(m2));
+  EXPECT_PRED1(isnan<double>, sum(v1));
+  EXPECT_PRED1(isnan<double>, sum(v2));
 }
