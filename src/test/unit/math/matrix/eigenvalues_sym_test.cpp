44b0d5a04d29e8be603b9246962819743a63a800
Marco Inacio <marcoigarapava@gmail.com>
2014-08-12 11:08:29 -0300

    added NaN test for eigenvalues_sym

--- src/test/unit/math/matrix/eigenvalues_sym_test.cpp
+++ src/test/unit/math/matrix/eigenvalues_sym_test.cpp
@@ -1,6 +1,7 @@
 #include <stan/math/matrix/eigenvalues_sym.hpp>
 #include <stan/math/matrix/typedefs.hpp>
 #include <gtest/gtest.h>
+#include <boost/math/special_functions/fpclassify.hpp>
 
 TEST(MathMatrix, eigenvalues_sym) {
   stan::math::matrix_d m0;
@@ -12,3 +13,29 @@ TEST(MathMatrix, eigenvalues_sym) {
   EXPECT_THROW(eigenvalues_sym(m1),std::domain_error);
 }
 
+TEST(MathMatrix,eigenvalues_sym_nan) {
+  double nan = std::numeric_limits<double>::quiet_NaN();
+
+  Eigen::MatrixXd m1(3,3);
+  m1 << 14, nan, 3,
+        nan, 10, 4.1,
+        3, 4.1, 8;
+        
+  Eigen::MatrixXd m2(3,3);
+  m2 << 10.1, 1, 1.3,
+        1, 5.4, 1.5,
+        1.3, 1.5, nan;
+  
+  Eigen::VectorXd vr;
+  
+  using stan::math::eigenvalues_sym;
+  using boost::math::isnan;
+
+  vr = eigenvalues_sym(m1);
+  for (int i = 0; i < vr.size(); i++)
+    EXPECT_PRED1(isnan<double>, vr(i));
+
+  vr = eigenvalues_sym(m2);
+  for (int i = 0; i < vr.size(); i++)
+    EXPECT_PRED1(isnan<double>, vr(i));
+}
