543d530ec3e9231ff96a3ac6a526d150a40661ef
Marco Inacio <marcoigarapava@gmail.com>
2014-08-13 10:49:02 -0300

    added NaN test for columns_dot_product

--- src/test/unit/math/matrix/columns_dot_product_test.cpp
+++ src/test/unit/math/matrix/columns_dot_product_test.cpp
@@ -1,2 +1,25 @@
 #include <stan/math/matrix/columns_dot_product.hpp>
 #include <gtest/gtest.h>
+#include <boost/math/special_functions/fpclassify.hpp>
+
+TEST(MathMatrix,columns_dot_product_nan) {
+  double nan = std::numeric_limits<double>::quiet_NaN();
+
+  Eigen::MatrixXd m1(2,3);
+  m1 << 14, nan, 3,
+        nan, 10, 4.1;
+        
+  Eigen::MatrixXd m2(2,3);
+  m2 << 10.1, 1, 1.3,
+        1, 5.4, 1.5;
+  
+  Eigen::RowVectorXd vr;
+  
+  using stan::math::columns_dot_product;
+  using boost::math::isnan;
+
+  vr = columns_dot_product(m1, m2);
+  EXPECT_PRED1(isnan<double>, vr(0));
+  EXPECT_PRED1(isnan<double>, vr(1));
+  EXPECT_DOUBLE_EQ(3*1.3 + 4.1*1.5, vr(2));
+}
