dc6030ea3e75e0f03599cc3ab8be6733beedac30
Marco Inacio <marcoigarapava@gmail.com>
2014-08-13 10:49:59 -0300

    added NaN test for columns_dot_self

--- src/test/unit/math/matrix/columns_dot_self_test.cpp
+++ src/test/unit/math/matrix/columns_dot_self_test.cpp
@@ -1,5 +1,6 @@
 #include <stan/math/matrix/columns_dot_self.hpp>
 #include <gtest/gtest.h>
+#include <boost/math/special_functions/fpclassify.hpp>
 
 TEST(MathMatrix,columns_dot_self) {
   using stan::math::columns_dot_self;
@@ -18,4 +19,22 @@ TEST(MathMatrix,columns_dot_self) {
   x = columns_dot_self(m3);
   EXPECT_NEAR(20.0,x(0,0),1E-12);
   EXPECT_NEAR(34.0,x(0,1),1E-12);
+}
+
+TEST(MathMatrix,columns_dot_self_nan) {
+  double nan = std::numeric_limits<double>::quiet_NaN();
+
+  Eigen::MatrixXd m1(2,3);
+  m1 << 14, nan, 3,
+        nan, 10, 4.1;
+  
+  Eigen::RowVectorXd vr;
+  
+  using stan::math::columns_dot_self;
+  using boost::math::isnan;
+
+  vr = columns_dot_self(m1);
+  EXPECT_PRED1(isnan<double>, vr(0));
+  EXPECT_PRED1(isnan<double>, vr(1));
+  EXPECT_DOUBLE_EQ(3*3 + 4.1*4.1, vr(2));
 }
