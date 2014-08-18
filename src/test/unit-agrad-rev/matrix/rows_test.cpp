bf144cab5fb6448dbfa21192b959a9341d2f2a66
Peter Li <Li.Peter94@gmail.com>
2014-08-18 17:03:03 -0400

    added NaN tests for rows

--- src/test/unit-agrad-rev/matrix/rows_test.cpp
+++ src/test/unit-agrad-rev/matrix/rows_test.cpp
@@ -37,3 +37,40 @@ TEST(AgradRevMatrix,rows_matrix) {
   m.resize(0,2);
   EXPECT_EQ(0U, rows(m));
 }
+
+TEST(AgradRevMatrix,rows_vector_nan) {
+  using stan::agrad::vector_v;
+  using stan::agrad::row_vector_v;
+  using stan::math::rows;
+  double nan = std::numeric_limits<double>::quiet_NaN();
+
+  vector_v v(5);
+  v << 0, nan, 2, nan, 4;
+  EXPECT_EQ(5U, rows(v));
+  
+  v.resize(0);
+  EXPECT_EQ(0U, rows(v));
+}
+TEST(AgradRevMatrix,rows_rowvector_nan) {
+  using stan::agrad::row_vector_v;
+  using stan::math::rows;
+  double nan = std::numeric_limits<double>::quiet_NaN();
+
+  row_vector_v rv(5);
+  rv << 0, nan, 2, nan, 4;
+  EXPECT_EQ(1U, rows(rv));
+
+  rv.resize(0);
+  EXPECT_EQ(1U, rows(rv));
+}
+TEST(AgradRevMatrix,rows_matrix_nan) {
+  using stan::agrad::matrix_v;
+  using stan::math::rows;
+
+  matrix_v m(2,3);
+  m << 0, nan, 2, 3, nan, 5;
+  EXPECT_EQ(2U, rows(m));
+  
+  m.resize(0,2);
+  EXPECT_EQ(0U, rows(m));
+}
