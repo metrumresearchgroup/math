de8c49a41a051e1a9636a6780e45b4f31e6196be
Marco Inacio <marcoigarapava@gmail.com>
2014-08-18 23:51:28 -0300

    added NaN test for row (based on Peter Li's agrad-rev test code)

--- src/test/unit/math/matrix/row_test.cpp
+++ src/test/unit/math/matrix/row_test.cpp
@@ -1,6 +1,7 @@
 #include <stan/math/matrix/row.hpp>
 #include <stan/math/matrix/typedefs.hpp>
 #include <gtest/gtest.h>
+#include <boost/math/special_functions/fpclassify.hpp>
 
 TEST(MathMatrix,row) {
   stan::math::matrix_d m(3,4);
@@ -21,4 +22,19 @@ TEST(MathMatrix, row_exception) {
 
   EXPECT_THROW(row(m1,5),std::domain_error);
   EXPECT_THROW(row(m1,0),std::domain_error);
+}
+
+TEST(MathMatrix,row_nan) {
+  using stan::math::row;
+  using stan::math::matrix_d;
+  using stan::math::vector_d;
+  double nan = std::numeric_limits<double>::quiet_NaN();
+
+  matrix_d y(2,3);
+  y << 1, 2, nan, 4, 5, 6;
+  vector_d z = row(y,1);
+  EXPECT_EQ(3,z.size());
+  EXPECT_FLOAT_EQ(1.0,z[0]);
+  EXPECT_FLOAT_EQ(2.0,z[1]);
+  EXPECT_TRUE(boost::math::isnan(z[2]));
 }
