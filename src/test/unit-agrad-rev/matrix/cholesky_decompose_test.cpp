07aa6c11cde267e483dbe02c22beb8855478044c
Peter Li <Li.Peter94@gmail.com>
2014-08-19 13:59:10 -0400

    added NaN test for cholesky_decompose

--- src/test/unit-agrad-rev/matrix/cholesky_decompose_test.cpp
+++ src/test/unit-agrad-rev/matrix/cholesky_decompose_test.cpp
@@ -33,4 +33,38 @@ TEST(AgradRevMatrix,mat_cholesky) {
 
   EXPECT_NO_THROW(singular_values(X));
 }
+TEST(AgradRevMatrix,mat_cholesky_nan) {
+  using stan::agrad::matrix_v;
+  using stan::math::transpose;
+  using stan::math::cholesky_decompose;
+  using stan::math::singular_values;
+  double nan = std::numeric_limits<double>::quiet_NaN();
+
+  // symmetric
+  matrix_v X(2,2);
+  AVAR a = 3.0;
+  AVAR b = nan;
+  AVAR c = -1.0;
+  AVAR d = 1.0;
+  X << a, b, 
+    c, d;
+  
+  matrix_v L = cholesky_decompose(X);
+
+  matrix_v LL_trans = multiply(L,transpose(L));
+  EXPECT_FLOAT_EQ(a.val(),LL_trans(0,0).val());
+  EXPECT_FLOAT_EQ(c.val(),LL_trans(0,1).val());
+  EXPECT_FLOAT_EQ(c.val(),LL_trans(1,0).val());
+  EXPECT_FLOAT_EQ(d.val(),LL_trans(1,1).val());
+
+  EXPECT_NO_THROW(singular_values(X));
+
+  X << a,c,b,d;
+  L = cholesky_decompose(X);
+  LL_trans = multiply(L,transpose(L));
+  EXPECT_FLOAT_EQ(a.val(),LL_trans(0,0).val());
+  EXPECT_TRUE(boost::math::isnan(LL_trans(0,1).val()));
+  EXPECT_TRUE(boost::math::isnan(LL_trans(1,0).val()));
+  EXPECT_TRUE(boost::math::isnan(LL_trans(1,1).val()));
+}
 
