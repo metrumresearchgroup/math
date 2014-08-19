880b5f561d26c295881c7f747c3523668f2ee396
Marco Inacio <marcoigarapava@gmail.com>
2014-08-18 23:41:41 -0300

    added NaN test for quad_form_diag (based on Peter Li's agrad-rev test code)

--- src/test/unit/math/matrix/quad_form_diag_test.cpp
+++ src/test/unit/math/matrix/quad_form_diag_test.cpp
@@ -1,6 +1,7 @@
 #include <stan/math/matrix/quad_form_diag.hpp>
 #include <test/unit/math/matrix/expect_matrix_eq.hpp>
 #include <gtest/gtest.h>
+#include <boost/math/special_functions/fpclassify.hpp>
 
 using Eigen::Matrix;
 using Eigen::Dynamic;
@@ -64,3 +65,38 @@ TEST(MathMatrix,quadFormDiagException) {
 }
 
 
+TEST(MathMatrix,quadFormDiag2_dv_nan) {
+  double nan = std::numeric_limits<double>::quiet_NaN();
+  Matrix<double,Dynamic,Dynamic> m(3,3);
+  Matrix<double,1,Dynamic> rv(3);
+  Matrix<double,Dynamic,1> v(3);
+
+  m << nan, 2, 3, 4, 5, 6, 7, 8, 9;
+  v << nan, 2, 3;
+  rv << nan, 2, 3;
+
+  Matrix<double, Dynamic, Dynamic> res1 = quad_form_diag(m, v);
+  Matrix<double, Dynamic, Dynamic> res2 = quad_form_diag(m, rv);
+
+  EXPECT_FLOAT_EQ(20, res1(1,1));
+  EXPECT_FLOAT_EQ(36, res1(1,2));
+  EXPECT_FLOAT_EQ(48, res1(2,1));
+  EXPECT_FLOAT_EQ(81, res1(2,2));
+  EXPECT_TRUE(boost::math::isnan(res1(0,0)));
+  EXPECT_TRUE(boost::math::isnan(res1(0,1)));
+  EXPECT_TRUE(boost::math::isnan(res1(0,2)));
+  EXPECT_TRUE(boost::math::isnan(res1(1,0)));
+  EXPECT_TRUE(boost::math::isnan(res1(2,0)));
+
+  EXPECT_FLOAT_EQ(20, res2(1,1));
+  EXPECT_FLOAT_EQ(36, res2(1,2));
+  EXPECT_FLOAT_EQ(48, res2(2,1));
+  EXPECT_FLOAT_EQ(81, res2(2,2));
+  EXPECT_TRUE(boost::math::isnan(res2(0,0)));
+  EXPECT_TRUE(boost::math::isnan(res2(0,1)));
+  EXPECT_TRUE(boost::math::isnan(res2(0,2)));
+  EXPECT_TRUE(boost::math::isnan(res2(1,0)));
+  EXPECT_TRUE(boost::math::isnan(res2(2,0)));
+  EXPECT_TRUE(boost::math::isnan(res2(2,0)));
+
+}
