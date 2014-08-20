c90d1f21a008d968b0c95ebb3d84998aa3655139
Marco Inacio <marcoigarapava@gmail.com>
2014-08-20 12:10:42 -0300

    added NaN test for tcrossprod

--- src/test/unit/math/matrix/tcrossprod_test.cpp
+++ src/test/unit/math/matrix/tcrossprod_test.cpp
@@ -1,5 +1,6 @@
 #include <stan/math/matrix/tcrossprod.hpp>
 #include <stan/math/matrix/typedefs.hpp>
+#include <boost/math/special_functions/fpclassify.hpp>
 #include <gtest/gtest.h>
 
 void test_tcrossprod(const stan::math::matrix_d& x) {
@@ -32,4 +33,48 @@ TEST(MathMatrix, tcrossprod) {
     2.0, 3.0, 0.0,
     4.0, 5.0, 6.0;
   test_tcrossprod(x);
+}
+
+TEST(MathMatrix,tcrossprod_nan) {
+  double nan = std::numeric_limits<double>::quiet_NaN();
+
+  Eigen::MatrixXd m1(3,2);
+  m1 << 1, nan,
+        3, 4.1,
+        nan, 6;
+  m1.transposeInPlace();
+  
+  Eigen::MatrixXd m2(3,2);
+  m2 << 10.1, 100,
+        nan, 0,
+        -10, -12.1;
+  m2.transposeInPlace();
+        
+  Eigen::RowVectorXd v1(2);
+  v1 << 11.1, nan;
+  
+  Eigen::MatrixXd mr;
+  
+  using stan::math::tcrossprod;
+  using boost::math::isnan;
+
+  mr = tcrossprod(m1);
+  for (int j = 0; j < mr.cols(); j++)
+    for (int i = 0; i < mr.rows(); i++)
+      EXPECT_PRED1(isnan<double>, mr(i, j));
+
+  mr = tcrossprod(m2);
+  EXPECT_PRED1(isnan<double>, mr(0, 0));
+  EXPECT_PRED1(isnan<double>, mr(0, 1));
+  EXPECT_PRED1(isnan<double>, mr(1, 0));
+  EXPECT_DOUBLE_EQ(10146.41, mr(1, 1)); //100^2+(-12.1)^2
+
+  mr = tcrossprod(v1);
+  EXPECT_PRED1(isnan<double>, mr(0, 0));
+
+  mr = tcrossprod(v1.transpose());
+  EXPECT_DOUBLE_EQ(123.21, mr(0, 0));
+  EXPECT_PRED1(isnan<double>, mr(0, 1));
+  EXPECT_PRED1(isnan<double>, mr(1, 0));
+  EXPECT_PRED1(isnan<double>, mr(1, 1));
 }
