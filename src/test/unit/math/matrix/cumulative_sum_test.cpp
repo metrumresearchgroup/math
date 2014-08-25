8a084e2b4bc23ba6e22894e4b99bb4208c602f19
Marco Inacio <marcoigarapava@gmail.com>
2014-08-25 15:31:01 -0300

    added NaN test for cumulative_sum

--- src/test/unit/math/matrix/cumulative_sum_test.cpp
+++ src/test/unit/math/matrix/cumulative_sum_test.cpp
@@ -1,4 +1,5 @@
 #include <stan/math/matrix/cumulative_sum.hpp>
+#include <boost/math/special_functions/fpclassify.hpp>
 #include <gtest/gtest.h>
 
 template <typename T>
@@ -41,4 +42,52 @@ TEST(MathMatrix, cumulative_sum) {
   test_cumulative_sum<std::vector<double> >();
   test_cumulative_sum<Eigen::Matrix<double,Eigen::Dynamic,1> >();
   test_cumulative_sum<Eigen::Matrix<double,1,Eigen::Dynamic> >();
+}
+
+TEST(MathMatrix, cumulative_sum_nan) {
+  double nan = std::numeric_limits<double>::quiet_NaN();
+
+  Eigen::MatrixXd m1(3,2);
+  m1 << 1, nan,
+        3.1, 4.1,
+        nan, 6;
+  Eigen::MatrixXd m2(3,2);
+  m2 << 10.1, 100,
+        nan, 0,
+        -10, -12;
+        
+  Eigen::VectorXd v1(3);
+  v1 << 100.1, nan, 1.1;
+        
+  std::vector<double> v2(14, 1.1);
+  v2[2] = nan;
+  
+  Eigen::MatrixXd mr;
+  std::vector<double> vr;
+        
+  using stan::math::cumulative_sum;
+  using boost::math::isnan;
+    
+  mr = cumulative_sum(m1);
+  EXPECT_DOUBLE_EQ(1, mr(0));
+  EXPECT_DOUBLE_EQ(4.1, mr(1));
+  for (int i = 2; i < 6; i++)
+    EXPECT_PRED1(isnan<double>, mr(i));
+    
+  mr = cumulative_sum(m2);
+  EXPECT_DOUBLE_EQ(10.1, mr(0));
+  for (int i = 1; i < 6; i++)
+    EXPECT_PRED1(isnan<double>, mr(i));
+    
+  mr = cumulative_sum(v1);
+  EXPECT_DOUBLE_EQ(100.1, mr(0));
+  for (int i = 1; i < 3; i++)
+    EXPECT_PRED1(isnan<double>, mr(i));
+    
+  vr = cumulative_sum(v2);
+  EXPECT_DOUBLE_EQ(1.1, vr[0]);
+  EXPECT_DOUBLE_EQ(2.2, vr[1]);
+  for (int i = 2; i < 14; i++)
+    EXPECT_PRED1(isnan<double>, vr[i]);
+
 }
