9b2d91b95c5a02a8187e6f69ec3d4191aac65ef5
Marco Inacio <marcoigarapava@gmail.com>
2014-08-25 13:02:52 -0300

    Added expect_matrix_nan for std::vector

--- src/test/unit/math/matrix/expect_matrix_nan.hpp
+++ src/test/unit/math/matrix/expect_matrix_nan.hpp
@@ -11,10 +11,20 @@ void expect_matrix_is_nan(const Eigen::Matrix<double, R, C> & mat) {
     EXPECT_PRED1(boost::math::isnan<double>, mat(i));
 }
 
+void expect_matrix_is_nan(const std::vector<double> & vec) {
+  for (size_t i = 0, size_ = vec.size(); i < size_; i++)
+    EXPECT_PRED1(boost::math::isnan<double>, vec[i]);
+}
+
 template <int R, int C>
 void expect_matrix_not_nan(const Eigen::Matrix<double, R, C> & mat) {
   for (int i = 0, size_ = mat.size(); i < size_; i++)
     EXPECT_FALSE(boost::math::isnan<double>(mat(i)));
+}
+
+void expect_matrix_not_nan(const std::vector<double> & vec) {
+  for (size_t i = 0, size_ = vec.size(); i < size_; i++)
+    EXPECT_FALSE(boost::math::isnan<double>(vec[i]));
 }
 
 #endif
