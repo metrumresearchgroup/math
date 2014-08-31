2d684f713d2419ff61c8832cabb762eface693f1
Marco Inacio <marcoigarapava@gmail.com>
2014-08-30 21:38:12 -0300

    added NaN test for inverse_spd

--- src/test/unit/math/matrix/inverse_spd_test.cpp
+++ src/test/unit/math/matrix/inverse_spd_test.cpp
@@ -23,3 +23,34 @@ TEST(MathMatrix, inverse_spd_exception) {
         3, 5, 6;
   EXPECT_THROW(inverse_spd(m1),std::domain_error);
 }
+
+TEST(MathMatrix, inverse_spd_nan) {
+  double nan = std::numeric_limits<double>::quiet_NaN();
+
+  Eigen::MatrixXd m0(3,3);
+  m0 << 100, 3.2, 0,
+        3.2, 100, nan,
+        0, nan, 100;
+
+  Eigen::MatrixXd m1(3,3);
+  m1 << 100, nan, 0.1,
+        nan, 100, 4.1,
+        0.1, 4.1, 100;
+        
+  Eigen::MatrixXd m2(3,3);
+  m2 << 100, 1.1, 3.2,
+        1.1, nan, 4.1,
+        3.2, 4.1, 100;
+          
+  Eigen::MatrixXd m3(3,3);
+  m3 << 100, 1.1, nan,
+        1.1, 100, 4.1,
+        nan, 4.1, 100;
+        
+  using stan::math::inverse_spd;
+    
+  EXPECT_THROW(inverse_spd(m0),std::domain_error);
+  EXPECT_THROW(inverse_spd(m1),std::domain_error);
+  EXPECT_THROW(inverse_spd(m2),std::domain_error);
+  EXPECT_THROW(inverse_spd(m3),std::domain_error);
+}
