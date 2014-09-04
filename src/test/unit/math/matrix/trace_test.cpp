5ee8163c95f82bd1e8ab868de2de83f4751dc620
Marco Inacio <marcoigarapava@gmail.com>
2014-09-04 14:43:53 -0300

    added NaN test for trace

--- src/test/unit/math/matrix/trace_test.cpp
+++ src/test/unit/math/matrix/trace_test.cpp
@@ -1,5 +1,6 @@
 #include <stan/math/matrix/trace.hpp>
 #include <stan/math/matrix/typedefs.hpp>
+#include <boost/math/special_functions/fpclassify.hpp>
 #include <gtest/gtest.h>
 
 TEST(MathMatrix, trace) {
@@ -12,4 +13,36 @@ TEST(MathMatrix, trace) {
   m = stan::math::matrix_d(2,3);
   m << 1, 2, 3, 4, 5, 6;
   EXPECT_FLOAT_EQ(6.0,trace(m));
+}
+
+TEST(MathMatrix, trace_nan) {
+  double nan = std::numeric_limits<double>::quiet_NaN();
+
+  Eigen::MatrixXd m1(3,2);
+  m1 << 1, nan,
+        nan, 4.1,
+        nan, nan;
+        
+  Eigen::MatrixXd m2(3,3);
+  m2 << 10.1, 100, 2,
+        1.0, 1, nan,
+        -10, -12, 6;
+
+  Eigen::MatrixXd m3(3,3);
+  m3 << 1, nan, 5.1,
+        3, 4.1, 1.1,
+        nan, 6, nan;
+        
+  Eigen::MatrixXd m4(3,2);
+  m4 << 10.1, 100,
+        0.1, nan,
+        -10, -12;
+
+  using stan::math::trace;
+  using boost::math::isnan;
+   
+  EXPECT_DOUBLE_EQ(5.1, trace(m1));
+  EXPECT_DOUBLE_EQ(17.1, trace(m2));
+  EXPECT_PRED1(isnan<double>, trace(m3));
+  EXPECT_PRED1(isnan<double>, trace(m4));
 }
