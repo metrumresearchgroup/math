82721dc86a59f7806a109719eb30f86a58f8ba8d
Marco Inacio <marcoigarapava@gmail.com>
2014-09-11 16:27:46 -0300

    added NaN test for diag_pre_multiply

--- src/test/unit/math/matrix/diag_pre_multiply_test.cpp
+++ src/test/unit/math/matrix/diag_pre_multiply_test.cpp
@@ -1,6 +1,6 @@
 #include <stan/math/matrix/diag_pre_multiply.hpp>
 #include <test/unit/math/matrix/expect_matrix_eq.hpp>
-#include <gtest/gtest.h>
+#include <test/unit/math/matrix/expect_matrix_nan.hpp>
 
 using Eigen::Matrix;
 using Eigen::Dynamic;
@@ -51,4 +51,24 @@ TEST(MathMatrix,diagPreMultiplyException) {
   EXPECT_THROW(diag_pre_multiply(v,m), std::domain_error);
 }
 
+TEST(MathMatrix, diag_pre_multiply_nan) {
+  double nan = std::numeric_limits<double>::quiet_NaN();
 
+  Eigen::MatrixXd m(3,3);
+  m << 1, nan, 1.1,
+       5.1, 4.1, 4.5,
+       nan, 6.1, 5.1;
+        
+  Eigen::VectorXd vm(3);
+  vm << 10.1, 6, nan;
+  
+  Eigen::MatrixXd mr(3,3);
+  mr << 10.1, nan, 11.11,
+        30.6, 24.6, 27,
+        nan, nan, nan;
+         
+  using stan::math::diag_pre_multiply;
+  using boost::math::isnan;
+  
+  expect_matrix_eq_or_both_nan(mr, diag_pre_multiply(vm, m));
+}
