7c34a8ba121112c11f5eecb1c3c6c64cfb3cfa73
Marco Inacio <marcoigarapava@gmail.com>
2014-09-11 14:19:48 -0300

    added NaN test for qr_R

--- src/test/unit/math/matrix/qr_R_test.cpp
+++ src/test/unit/math/matrix/qr_R_test.cpp
@@ -2,7 +2,7 @@
 #include <stan/math/matrix/qr_R.hpp>
 #include <stan/math/matrix/typedefs.hpp>
 #include <stan/math/matrix/transpose.hpp>
-#include <gtest/gtest.h>
+#include <test/unit/math/matrix/expect_matrix_nan.hpp>
 
 TEST(MathMatrix, qr_R) {
   stan::math::matrix_d m0(0,0);
@@ -23,4 +23,38 @@ TEST(MathMatrix, qr_R) {
     }
   }
   EXPECT_THROW(qr_R(transpose(m1)),std::domain_error);
+}
+
+TEST(MathMatrix,qr_R_nan) {
+  double nan = std::numeric_limits<double>::quiet_NaN();
+        
+  Eigen::MatrixXd m1(2,2);
+  m1 << 14, nan,
+        nan, 10;
+        
+  Eigen::MatrixXd m2(2,2);
+  m2 << nan, 1,
+        3, 5.1;
+        
+  Eigen::MatrixXd m3(2,2);
+  m3 << 3, 1,
+        3, nan;
+  
+  Eigen::MatrixXd m3_(2,2);
+  m3_ << 3, 1,
+         3, 100;
+  
+  Eigen::MatrixXd mr(2, 2);
+  mr << nan, nan,
+        0, nan;
+  
+  using stan::math::qr_R;
+  using boost::math::isnan;
+
+  expect_matrix_eq_or_both_nan(qr_R(m1), mr);
+  expect_matrix_eq_or_both_nan(qr_R(m2), mr);
+  
+  mr << qr_R(m3_)(0,0), nan,
+        0, nan;
+  expect_matrix_eq_or_both_nan(qr_R(m3), mr);
 }
