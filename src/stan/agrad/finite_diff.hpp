ddf0c2930fcb9cb106de111523471367a760cf6c
Rob Trangucci <robert.trangucci@gmail.com>
2015-01-29 19:41:40 -0500

    Final finite_diff functions and tests

--- src/stan/agrad/finite_diff.hpp
+++ src/stan/agrad/finite_diff.hpp
@@ -11,6 +11,30 @@ namespace stan {
   
   namespace agrad {
 
+    template <typename F>
+    double
+    finite_diff_hess_helper(const F& f,
+                            const Eigen::Matrix<double,Eigen::Dynamic,1>& x,
+                            const int& lambda,
+                            const double epsilon = 1e-03) {
+      Eigen::Matrix<double,Eigen::Dynamic,1> x_temp(x);
+
+      double grad = 0.0;
+      x_temp(lambda) = x(lambda) + 2.0 * epsilon;
+      grad = -f(x_temp);
+
+      x_temp(lambda) = x(lambda) + -2.0 * epsilon;
+      grad += f(x_temp);
+
+      x_temp(lambda) = x(lambda) + epsilon;
+      grad += 8.0 * f(x_temp);
+
+      x_temp(lambda) = x(lambda) + -epsilon;
+      grad -= 8.0 * f(x_temp);
+
+      return grad;
+    }
+
     /**
      * Calculate the value and the gradient of the specified function
      * at the specified argument using finite difference.  
@@ -40,26 +64,37 @@ namespace stan {
                          const Eigen::Matrix<double,Eigen::Dynamic,1>& x,
                          double& fx,
                          Eigen::Matrix<double,Eigen::Dynamic,1>& grad_fx, 
-                         const double epsilon = 1e-6) {
+                         const double epsilon = 1e-03) {
       Eigen::Matrix<double,Eigen::Dynamic,1> x_temp(x);
 
-      size_type d = x.size();
+      int d = x.size();
       grad_fx.resize(d);
 
       fx = f(x);
       
-      for (size_type i = 0; i < d; ++i){
+      for (int i = 0; i < d; ++i){
         double delta_f = 0.0;
 
-        x_temp(i) += epsilon;
+        x_temp(i) += 3.0 * epsilon;
         delta_f = f(x_temp);
 
-        x_temp(i) = x(i) - epsilon;
-        delta_f -= f(x_temp);
-        
-        x_temp(i) = x(i);
+        x_temp(i) = x(i) + 2.0 * epsilon;
+        delta_f -= 9.0 * f(x_temp);
 
-        delta_f /= 2.0 * epsilon;
+        x_temp(i) = x(i) + epsilon;
+        delta_f += 45.0 * f(x_temp);
+
+        x_temp(i) = x(i) + -3.0 * epsilon;
+        delta_f -= f(x_temp);
+
+        x_temp(i) = x(i) + -2.0 * epsilon;
+        delta_f += 9.0 * f(x_temp);
+
+        x_temp(i) = x(i) + -epsilon;
+        delta_f -= 45.0 * f(x_temp);
+
+        delta_f /= 60 * epsilon;
+        x_temp(i) = x(i);
         grad_fx(i) = delta_f;
       }
 
@@ -71,9 +106,9 @@ namespace stan {
                         const Eigen::Matrix<double,Eigen::Dynamic,1>& x,
                         double& fx,
                         Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic>& hess_fx, 
-                        const double epsilon = 1e-6) {
+                        const double epsilon = 1e-03) {
 
-      size_type d = x.size();
+      int d = x.size();
 
       Eigen::Matrix<double,Eigen::Dynamic,1> x_temp(x);
       hess_fx.resize(d, d);
@@ -82,31 +117,33 @@ namespace stan {
       
       double f_diff(0.0);
       for (int i = 0; i < d; ++i){
-        for (int j = 0; j < d; ++j){
-          x_temp(i) += epsilon;
+        for (int j = i; j < d; ++j){
+          x_temp(i) += 2.0 * epsilon;
           if(i != j){
-            f_diff = 0;
-            x_temp(j) += epsilon;
-            f_diff += f(x_temp);
-            x_temp(j) = x(j) - epsilon;
-            f_diff -= f(x_temp);
-            x_temp(i) = x(i) - epsilon;
-            f_diff += f(x_temp);
-            x_temp(j) = x(j) + epsilon;
-            f_diff -= f(x_temp);
-            f_diff /= 4;
-            x_temp(j) = x(j);
+            f_diff = -finite_diff_hess_helper(f,x_temp,j);
+            x_temp(i) = x(i) + -2.0 * epsilon;
+            f_diff += finite_diff_hess_helper(f,x_temp,j);
+            x_temp(i) = x(i) + epsilon;
+            f_diff += 8.0 * finite_diff_hess_helper(f,x_temp,j);
+            x_temp(i) = x(i) + -epsilon;
+            f_diff -= 8.0 * finite_diff_hess_helper(f,x_temp,j);
+            f_diff /= 12.0 * epsilon * 12.0 * epsilon;
           } else {
-            f_diff = -2 * fx;
-            f_diff += f(x_temp);
+            f_diff = -f(x_temp);
+            f_diff -= 30 * fx;
+            x_temp(i) = x(i) + -2.0 * epsilon;
+            f_diff -= f(x_temp);
+            x_temp(i) = x(i) + epsilon;
+            f_diff += 16.0 * f(x_temp);
             x_temp(i) = x(i) - epsilon;
-            f_diff += f(x_temp);
+            f_diff += 16.0 * f(x_temp);
+            f_diff /= 12 * epsilon * epsilon;
           }
 
           x_temp(i) = x(i);
-          f_diff /= epsilon * epsilon;
           
           hess_fx(j,i) = f_diff;
+          hess_fx(i,j) = hess_fx(j,i);
         }
       }
     }
@@ -140,9 +177,9 @@ namespace stan {
                              const Eigen::Matrix<double,Eigen::Dynamic,1>& x,
                              double& fx,
                              Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic>& hess_fx, 
-                             const double epsilon = 1e-6) {
+                             const double epsilon = 1e-03) {
 
-      size_type d = x.size();
+      int d = x.size();
 
       Eigen::Matrix<double,Eigen::Dynamic,1> x_temp(x);
       Eigen::Matrix<double,Eigen::Dynamic,1> g_auto(d);
@@ -150,18 +187,26 @@ namespace stan {
 
       fx = f(x);
       
-      for (size_type i = 0; i < d; ++i){
+      for (int i = 0; i < d; ++i){
         Eigen::VectorXd g_diff = Eigen::VectorXd::Zero(d);
-        x_temp(i) += epsilon;
+        x_temp(i) += 2.0 * epsilon;
         gradient(f, x_temp, fx, g_auto);
-        g_diff = g_auto;
+        g_diff = -g_auto;
+
+        x_temp(i) = x(i) + -2.0 * epsilon;
+        gradient(f, x_temp, fx, g_auto);
+        g_diff += g_auto;
+
+        x_temp(i) = x(i) + epsilon;
+        gradient(f, x_temp, fx, g_auto);
+        g_diff += 8.0 * g_auto;
 
         x_temp(i) = x(i) - epsilon;
         gradient(f, x_temp, fx, g_auto);
-        g_diff -= g_auto;
+        g_diff -= 8.0 * g_auto;
 
         x_temp(i) = x(i);
-        g_diff /= 2.0 * epsilon;
+        g_diff /= 12.0 * epsilon;
         
         hess_fx.col(i) = g_diff;
       }
@@ -170,13 +215,13 @@ namespace stan {
     
     template <typename F>
     void
-    finite_diff_grad_hessian(const F& f,
-                             const Eigen::Matrix<double,Eigen::Dynamic,1>& x,
-                             double& fx,
-                             std::vector<Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic> >& grad_hess_fx, 
-                             const double epsilon = 1e-6) {
+    finite_diff_grad_hessian_auto(const F& f,
+                                  const Eigen::Matrix<double,Eigen::Dynamic,1>& x,
+                                  double& fx,
+                                  std::vector<Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic> >& grad_hess_fx, 
+                                  const double epsilon = 1e-04) {
 
-      size_type d = x.size();
+      int d = x.size();
 
       Eigen::Matrix<double,Eigen::Dynamic,1> x_temp(x);
       Eigen::Matrix<double,Eigen::Dynamic,1> grad_auto(d);
@@ -185,19 +230,27 @@ namespace stan {
 
       fx = f(x);
       
-      for (size_type i = 0; i < d; ++i){
+      for (int i = 0; i < d; ++i){
         H_diff.setZero();
 
-        x_temp(i) += epsilon;
+        x_temp(i) += 2.0 * epsilon;
         hessian(f, x_temp, fx, grad_auto, H_auto);
-        H_diff = H_auto;
+        H_diff = -H_auto;
 
-        x_temp(i) = x(i) - epsilon;
+        x_temp(i) = x(i) + -2.0 * epsilon;
         hessian(f, x_temp, fx, grad_auto, H_auto);
-        H_diff -= H_auto;
+        H_diff += H_auto;
+
+        x_temp(i) = x(i) + epsilon;
+        hessian(f, x_temp, fx, grad_auto, H_auto);
+        H_diff += 8.0 * H_auto;
+
+        x_temp(i) = x(i) + -epsilon;
+        hessian(f, x_temp, fx, grad_auto, H_auto);
+        H_diff -= 8.0 * H_auto;
 
         x_temp(i) = x(i);
-        H_diff /= 2.0 * epsilon;
+        H_diff /= 12.0 * epsilon;
         
         grad_hess_fx.push_back(H_diff);
       }
--- src/test/unit/agrad/finite_diff_test.cpp
+++ src/test/unit/agrad/finite_diff_test.cpp
@@ -75,88 +75,95 @@ norm_hess(const Eigen::Matrix<double,Eigen::Dynamic,1>& x){
 }
 
 
-//TEST(AgradFiniteDiff,gradient) {
-//  using Eigen::Matrix;
-//  using Eigen::Dynamic;
-//
-//  fun1 f;
-//  norm1 n;
-//  Matrix<double,Dynamic,1> x(2);
-//  Matrix<double,Dynamic,1> norm_vec(3);
-//
-//  x << 0.5, 0.3;
-//
-//
-//  norm_vec << 0.5, 0.3, 0.7;
-//  double fx;
-//  double fin_diff_fx;
-//
-//  Matrix<double,Dynamic,1> grad_fx;
-//  Matrix<double,Dynamic,1> finite_diff_fx;
-//
-//  Matrix<double,Dynamic,1> grad_norm_fx;
-//  Matrix<double,Dynamic,1> finite_diff_norm_fx;
-//
-//  stan::agrad::gradient(f,x,fx,grad_fx);
-//  stan::agrad::finite_diff_gradient(f,x,fin_diff_fx,finite_diff_fx);
-//  EXPECT_NEAR(finite_diff_fx(0), grad_fx(0), 1e-06);
-//  EXPECT_NEAR(finite_diff_fx(1), grad_fx(1), 1e-06);
-//
-//  stan::agrad::gradient(n,norm_vec,fx,grad_norm_fx);
-//  stan::agrad::finite_diff_gradient(n,norm_vec,fx,finite_diff_norm_fx);
-//
-//  for (size_type i = 0; i < 3; ++i)
-//    EXPECT_NEAR(grad_norm_fx(i), finite_diff_norm_fx(i), 1e-06);
-//
-//}
-//
-//TEST(AgradFiniteDiff,hessian) {
-//  using Eigen::Matrix;  
-//  using Eigen::Dynamic;
-//  fun1 f;
-//  norm1 n;
-//  Matrix<double,Dynamic,1> x(2);
-//  Matrix<double,Dynamic,1> norm_vec(3);
-//
-//  Matrix<double,2,2> fun1_hess;
-//
-//  x << 0.5, 0.7;
-//
-//  fun1_hess << 2 * x(1), 2 * x(0),
-//               2 * x(0), 6;
-//
-//  norm_vec << 0.5, 0.3, 0.7;
-//
-//  double fx(0);
-//  double finite_diff_x(0);
-//  Matrix<double,Dynamic,1> grad;
-//  Matrix<double,Dynamic,Dynamic> H;
-//  Matrix<double,Dynamic,Dynamic> finite_diff_H;
-//
-//  Matrix<double,Dynamic,1> norm_grad;
-//  Matrix<double,Dynamic,Dynamic> norm_H;
-//  Matrix<double,Dynamic,Dynamic> finite_diff_norm_H;
-//  stan::agrad::hessian(f,x,fx,grad,H);
-//  stan::agrad::finite_diff_hessian(f,x,finite_diff_x,finite_diff_H);
-//
-//  stan::agrad::hessian(n,norm_vec,fx,norm_grad,norm_H);
-//  stan::agrad::finite_diff_hessian(n,norm_vec,finite_diff_x,finite_diff_norm_H);
-//
-//  Matrix<double,3,3> norm_hess_an = norm_hess(norm_vec);
-//
-//  for (size_type i = 0; i < 2; ++i)
-//    for (size_type j = 0; j < 2; ++j){
-//      EXPECT_NEAR(H(i,j), finite_diff_H(i,j), 3e-04);
-//      EXPECT_NEAR(fun1_hess(i,j), finite_diff_H(i,j),3e-04);
-//    }
-//
-//  for (size_type i = 0; i < 3; ++i)
-//    for (size_type j = 0; j < 3; ++j){
-//      EXPECT_NEAR(norm_H(i,j), finite_diff_norm_H(i,j), 3e-04) << "i: " << i << " j: " << j;
-//      EXPECT_NEAR(norm_hess_an(i,j), finite_diff_norm_H(i,j), 3e-04) << "i: " << i << " j: " << j;
-//    }
-//
-//}
+TEST(AgradFiniteDiff,gradient) {
+  using Eigen::Matrix;
+  using Eigen::Dynamic;
+
+  fun1 f;
+  norm1 n;
+  Matrix<double,Dynamic,1> x(3);
+  Matrix<double,Dynamic,1> norm_vec(3);
+
+  x << 0.5, 0.3, 0.1;
+  norm_vec << 0.5, 0.3, 0.7;
+
+  double fx;
+  double fin_diff_fx;
+
+  Matrix<double,Dynamic,1> grad_fx;
+  Matrix<double,Dynamic,1> finite_diff_fx;
+
+  Matrix<double,Dynamic,1> grad_norm_fx;
+  Matrix<double,Dynamic,1> finite_diff_norm_fx;
+
+  stan::agrad::gradient(f,x,fx,grad_fx);
+  stan::agrad::finite_diff_gradient(f,x,fin_diff_fx,finite_diff_fx);
+
+  stan::agrad::gradient(n,norm_vec,fx,grad_norm_fx);
+  stan::agrad::finite_diff_gradient(n,norm_vec,fx,finite_diff_norm_fx);
+
+  for (size_type i = 0; i < 3; ++i){
+    EXPECT_NEAR(grad_norm_fx(i), finite_diff_norm_fx(i), 1e-12);
+    EXPECT_NEAR(grad_fx(i), finite_diff_fx(i), 1e-12);
+  }
+
+}
+
+TEST(AgradFiniteDiff,hessian) {
+  using Eigen::Matrix;  
+  using Eigen::Dynamic;
+  fun1 f;
+  norm1 n;
+  Matrix<double,Dynamic,1> x(3);
+  Matrix<double,Dynamic,1> norm_vec(3);
+
+  Matrix<double,3,3> fun1_hess;
+
+  x << 0.5, 0.7, 0.4;
+
+  fun1_hess << 6 * x(1) * x(1) * x(0), 6 * x(0) * x(0) * x(1) + 3 * x(1) * x(1) + x(2) * x(2) * x(2), 3 * x(2) * x(2) * x(1),
+               6 * x(0) * x(0) * x(1) + 3 * x(1) * x(1) + x(2) * x(2) * x(2), 2 * x(0) * x(0) * x(0) + 6 * x(1) * x(0), 3 * x(2) * x(2) * x(0),
+               3 * x(2) * x(2) * x(1), 3 * x(2) * x(2) * x(0), 6 * x(0) * x(1) * x(2);
+
+  norm_vec << 0.5, 0.3, 0.7;
+
+  double fx(0);
+  double finite_diff_x(0);
+  Matrix<double,Dynamic,1> grad;
+  Matrix<double,Dynamic,Dynamic> H;
+  Matrix<double,Dynamic,Dynamic> finite_diff_H;
+  Matrix<double,Dynamic,Dynamic> finite_diff_H_auto;
+
+  Matrix<double,Dynamic,1> norm_grad;
+  Matrix<double,Dynamic,Dynamic> norm_H;
+  Matrix<double,Dynamic,Dynamic> finite_diff_norm_H;
+  Matrix<double,Dynamic,Dynamic> finite_diff_norm_H_auto;
+
+  stan::agrad::hessian(f,x,fx,grad,H);
+  stan::agrad::finite_diff_hessian(f,x,finite_diff_x,finite_diff_H);
+  stan::agrad::finite_diff_hessian_auto(f,x,finite_diff_x,finite_diff_H_auto);
+
+  stan::agrad::hessian(n,norm_vec,fx,norm_grad,norm_H);
+  stan::agrad::finite_diff_hessian(n,norm_vec,finite_diff_x,finite_diff_norm_H);
+  stan::agrad::finite_diff_hessian(n,norm_vec,finite_diff_x,finite_diff_norm_H_auto);
+
+  Matrix<double,3,3> norm_hess_an = norm_hess(norm_vec);
+
+  for (size_type i = 0; i < 3; ++i)
+    for (size_type j = 0; j < 3; ++j){
+      EXPECT_NEAR(H(i,j), finite_diff_H(i,j), 1e-09);
+      EXPECT_NEAR(fun1_hess(i,j), finite_diff_H(i,j),1e-09) << "i: " << i << " j: " << j;
+      EXPECT_NEAR(fun1_hess(i,j), finite_diff_H_auto(i,j),1e-09) << "i: " << i << " j: " << j;
+    }
+
+  for (size_type i = 0; i < 3; ++i)
+    for (size_type j = 0; j < 3; ++j){
+      EXPECT_NEAR(norm_H(i,j), finite_diff_norm_H(i,j), 1e-09) << "i: " << i << " j: " << j;
+      EXPECT_NEAR(norm_hess_an(i,j), finite_diff_norm_H(i,j), 1e-09) << "i: " << i << " j: " << j;
+      EXPECT_NEAR(norm_hess_an(i,j), finite_diff_norm_H_auto(i,j), 1e-09) << "i: " << i << " j: " << j;
+    }
+
+}
 
 TEST(AgradFiniteDiff,grad_hessian) {
   using Eigen::Matrix;  
@@ -182,25 +189,18 @@ TEST(AgradFiniteDiff,grad_hessian) {
     grad_hess_fun1(fun_vec);
 
   stan::agrad::grad_hessian(fun,fun_vec,fx,norm_H_ad,ad_grad_H);
-  stan::agrad::finite_diff_grad_hessian(fun,fun_vec,finite_diff_x,finite_diff_norm_H);
+  stan::agrad::finite_diff_grad_hessian_auto(fun,fun_vec,finite_diff_x,finite_diff_norm_H);
 
 
   for (size_t i = 0; i < 3; ++i){
     for (size_type j = 0; j < 3; ++j){
       for (size_type k = 0; k < 3; ++k){
-        EXPECT_NEAR(ad_grad_H[i](j,k),finite_diff_norm_H[i](j,k),1e-03) << " i: " << i << " j: " << j << " k: " << k; 
-        EXPECT_NEAR(ad_grad_H[i](j,k),an_norm_H[i](j,k),1e-07) << " i: " << i << " j: " << j << " k: " << k; 
+        EXPECT_NEAR(ad_grad_H[i](j,k),finite_diff_norm_H[i](j,k),1e-10) << " i: " << i << " j: " << j << " k: " << k; 
+        EXPECT_NEAR(an_norm_H[i](j,k),finite_diff_norm_H[i](j,k),1e-10) << " i: " << i << " j: " << j << " k: " << k; 
+        EXPECT_NEAR(ad_grad_H[i](j,k),an_norm_H[i](j,k),1e-10) << " i: " << i << " j: " << j << " k: " << k; 
       }
     }
   }
 
-//  for (size_t i = 0; i < 3; ++i){
-//    for (size_type j = 0; j < 3; ++j){
-//      for (size_type k = 0; k < 3; ++k)
-//        std::cout << ad_grad_H[i](j,k) << " "; 
-//      std::cout << std::endl;
-//    }
-//    std::cout << std::endl;
-//  }
 
 }
