7ac6fc1b7a2bdb3acd1e5b87f5ad4097469cddf9
Yi Zhang <yiz@metrumrg.com>
2018-06-07 16:48:25 -0400

    more debug printing

--- stan/math/torsten/debug.hpp
+++ stan/math/torsten/debug.hpp
@@ -18,25 +18,46 @@ template class std::vector<std::vector<stan::math::var>> ;
 
 namespace torsten {
   template<typename T, typename D>
-  void debug_grad(T& f, std::vector<D> & x, std::vector<double>& g) {}
+  void debug_grad(const T& f, const std::vector<D> & x, std::vector<double>& g) {
+    std::cout << "N/A" << "\n";
+  }
   template<typename T, typename D>
-  void debug_grad(T& f, D & x, std::vector<double>& g) {}
-
-  void debug_grad(stan::math::var& f,
-                  std::vector<stan::math::var>& x,
-                  std::vector<double>& g) {
-    f.grad(x, g);
-    stan::math::set_zero_all_adjoints();
+  void debug_grad(const T& f, const D& x, std::vector<double>& g) {
+    std::cout << "N/A" << "\n";
   }
 
-  void debug_grad(stan::math::var& f,
-                  stan::math::var& x,
+  void debug_grad(const stan::math::var& f,
+                  const std::vector<stan::math::var>& x,
                   std::vector<double>& g) {
-    std::vector<stan::math::var> xv{x};
-    f.grad(xv, g);
+    stan::math::var ff = f;
+    std::vector<stan::math::var> xx {x};
+    ff.grad(xx, g);
     stan::math::set_zero_all_adjoints();
+    Eigen::Matrix<double, 1, Eigen::Dynamic> gm =
+      stan::math::to_matrix(g, 1, g.size());
+    std::cout << gm << "\n";
+  }
+
+  void debug_grad(const stan::math::var& f,
+                  const stan::math::var& x,
+                  std::vector<double>& g) {
+    stan::math::var ff = f;
+    std::vector<stan::math::var> xv{x};
+    ff.grad(xv, g);
+    stan::math::set_zero_all_adjoints();
+    std::cout << g[0] << "\n";
   }  
 }
+
+#ifndef TORSTEN_DEBUG_DIFF
+#define TORSTEN_DEBUG_DIFF(F, X, G)             \
+  std::cout << "TORSTEN DEBUG: ";               \
+  std::cout << std::setw(35);                   \
+  std::cout << "d[ "#F" ] / d[ "#X" ] ";        \
+  std::cout << std::setw(5);                    \
+  std::cout << " = ";                           \
+  debug_grad(F, X, G);
+#endif
 
 #endif
 
