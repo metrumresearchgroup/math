33e750983fe960095eeeae7a67e97ef317d9893a
Yi Zhang <yiz@metrumrg.com>
2019-02-04 12:18:50 -0800

    add PkAdams integrator for PK ODE models

--- stan/math/torsten/pk_ode_integrator.hpp
+++ stan/math/torsten/pk_ode_integrator.hpp
@@ -193,7 +193,7 @@ namespace torsten {
   };
 
   /*
-   * specialization
+   * specialization for @c PkBdf
    */
   template<>
   struct PkOdeIntegrator<PkBdf> {
@@ -241,6 +241,69 @@ namespace torsten {
             const std::vector<double>& x_r,
             const std::vector<int>& x_i) const {
       using Ode = torsten::dsolve::PKCvodesFwdSystem<F, Tt, T_initial, T_param, CV_BDF, AD>;
+      const int m = theta.size();
+      const int n = y0.size();
+
+      dsolve::PKCvodesService<typename Ode::Ode> serv(n, m);
+
+      Ode ode{serv, f, t0, ts, y0, theta, x_r, x_i, msgs};
+
+      torsten::dsolve::PKCvodesIntegrator solver(rtol, atol, max_num_step);
+      Eigen::MatrixXd res = solver.integrate<Ode, false>(ode);
+
+      return res;
+    }
+  };
+
+  /*
+   * specialization for @c PkAdams
+   */
+  template<>
+  struct PkOdeIntegrator<PkAdams> {
+    const double rtol;
+    const double atol;
+    const long int max_num_step;
+    std::ostream* msgs;
+
+    PkOdeIntegrator() : rtol(1e-10), atol(1e-10), max_num_step(1e8), msgs(0) {}
+
+    PkOdeIntegrator(const double rtol0, const double atol0,
+                    const long int max_num_step0,
+                    std::ostream* msgs0) :
+      rtol(rtol0), atol(atol0), max_num_step(max_num_step0), msgs(msgs0)
+    {}
+
+    /*
+     * Torsten's ODE solvers support @c T_t to be @c var
+     */
+    template <typename F, typename Tt, typename T_initial, typename T_param>
+    std::vector<std::vector<typename stan::return_type<Tt, T_initial, T_param>::type> > // NOLINT
+    operator()(const F& f,
+               const std::vector<T_initial>& y0,
+               double t0,
+               const std::vector<Tt>& ts,
+               const std::vector<T_param>& theta,
+               const std::vector<double>& x_r,
+               const std::vector<int>& x_i) const {
+      using internal::PkOdeIntegratorDispatcher;
+      return PkOdeIntegratorDispatcher<PkAdams>()(f, y0, t0, ts, theta, x_r, x_i,
+                                                msgs, rtol, atol, max_num_step);
+    }
+
+    /*
+     * For MPI solution we need to return data that consists
+     * of solution value and gradients
+     */
+    template <typename F, typename Tt, typename T_initial, typename T_param>
+    Eigen::MatrixXd
+    solve_d(const F& f,
+            const std::vector<T_initial>& y0,
+            double t0,
+            const std::vector<Tt>& ts,
+            const std::vector<T_param>& theta,
+            const std::vector<double>& x_r,
+            const std::vector<int>& x_i) const {
+      using Ode = torsten::dsolve::PKCvodesFwdSystem<F, Tt, T_initial, T_param, CV_ADAMS, AD>;
       const int m = theta.size();
       const int n = y0.size();
 
