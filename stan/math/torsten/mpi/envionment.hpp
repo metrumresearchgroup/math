dcafec1ed244c95a2847c93b0a2cadc3db762d04
Yi Zhang <yiz@metrumrg.com>
2019-04-03 07:29:17 -0700

    add OpenMP ifdef for MPI init

--- stan/math/torsten/mpi/envionment.hpp
+++ stan/math/torsten/mpi/envionment.hpp
@@ -1,6 +1,7 @@
 #ifndef STAN_MATH_TORSTEN_MPI_ENVIONMENT_HPP
 #define STAN_MATH_TORSTEN_MPI_ENVIONMENT_HPP
 
+#include <stan/math/prim/scal/err/check_greater_or_equal.hpp>
 #ifdef TORSTEN_MPI
 #include <boost/mpi.hpp>
 #endif
@@ -19,7 +20,16 @@ namespace torsten {
 #ifdef TORSTEN_MPI
         int flag;
         MPI_Initialized(&flag);
-        if(!flag) MPI_Init(NULL, NULL);
+        if(!flag) {
+#ifdef _OPENMP
+          static const char* caller("torsten::mpi::init");
+          int provided;
+          MPI_Init_thread(NULL, NULL, MPI_THREAD_FUNNELED, &provided);
+          stan::math::check_greater_or_equal(caller, "level of threads support", provided, MPI_THREAD_FUNNELED); // NOLINT
+#else
+          MPI_Init(NULL, NULL);
+#endif
+        }
 #endif
       }
 
